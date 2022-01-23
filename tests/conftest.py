0bd4aaeb9b7b12138537e1de09dfd8c305790149
Fabian Peter Hammerle <fabian@hammerle.me>
2022-01-23 10:47:25 +0100

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,15 @@
 TEST_PMID = '31025164'
 
+# PMID invalid (expecting 31025164)
 TEST_BIBTEX_ENTRY = r"""@Article{pmid31025164,
-   Author="Egger, F.  and Hofer, C.  and Hammerle, F. P.  and Lofler, S.  and Nurnberg, M.  and Fiedler, L.  and Kriz, R.  and Kern, H.  and Huber, K. ",
+   Author="Egger, F.  and Hofer, C.  and Hammerle, F. P.  and Löfler, S.  and Nürnberg, M.  and Fiedler, L.  and Kriz, R.  and Kern, H.  and Huber, K. ",
    Title="{{I}nfluence of electrical stimulation therapy on permanent pacemaker function}",
-   Journal="Wien. Klin. Wochenschr.",
+   Journal="Wien Klin Wochenschr",
    Year="2019",
    Volume="131",
    Number="13-14",
    Pages="313--320",
    Month="Jul",
-   Note={[DOI:\href{https://dx.doi.org/10.1007/s00508-019-1494-5}{10.1007/s00508-019-1494-5}] [PubMed:\href{https://www.ncbi.nlm.nih.gov/pubmed/31025164}{31025164}] }
+   Note={[DOI:\href{https://dx.doi.org/10.1007/s00508-019-1494-5}{10.1007/s00508-019-1494-5}] [PubMed:\href{https://www.ncbi.nlm.nih.gov/pubmed/9793098}{9793098}] }
 }
 """
